00a712d6e051fa4a7182c32f361450ad0b755218
Jairo <paypal061994@gmail.com>
2023-12-25 21:30:41 +0000

    adicionando domínio a ALLOWED_HOSTS

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@r-hxp3q4owm&*nt=yupan2w%&_d4a$ypz$q5qzbdvyhuh#e(%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['meutodoapp.cloudns.biz', 'localhost', '127.0.0.1']
 
 
 # Application definition
